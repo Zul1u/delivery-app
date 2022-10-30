61a2cd9ec4d3373376e576362bc7660c9abbfd03
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-30 16:28:31 -0300

    feat(app): injected express.static

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,6 +4,8 @@ const errorMiddleware = require('./middlewares/error.middleware');
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.use(express.json());
 
 // app.get('/coffee', (_req, res) => res.status(418).end());
