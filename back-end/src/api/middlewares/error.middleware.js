312252036b2d3037b92bc4057fc223fba78e492d
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-31 11:22:13 -0300

    feat: added error stack to errorHandler response

--- back-end/src/api/middlewares/error.middleware.js
+++ back-end/src/api/middlewares/error.middleware.js
@@ -1,13 +1,13 @@
 const { StatusCodes } = require('http-status-codes');
 
 const errorMiddleware = (error, _req, res, _next) => {
-  const { statusCode, message } = error;
+  const { statusCode, message, stack } = error;
 
   if (statusCode) {
     return res.status(statusCode).json({ message });
   }
 
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Corram para as colinas.', error });
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Corram para as colinas!', stack, error });
 };
 
 module.exports = errorMiddleware;
