4823a3eab45b0cf8719128ab3b5258fd01ce74b9
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-31 09:51:50 -0300

    feat: created data file with static info

---
+++ back-end/src/api/utils/staticData.js
@@ -0,0 +1,17 @@
+const userRoles = {
+  admin: 'admin',
+  customer: 'customer',
+  seller: 'seller',
+};
+
+const saleStatuses = {
+  pendente: 'Pendente',
+  preparando: 'Preparando',
+  emTransito: 'Em Trânsito',
+  entregue: 'Entregue',
+};
+
+module.exports = {
+  userRoles,
+  saleStatuses,
+};
