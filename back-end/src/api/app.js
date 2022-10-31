8e4bf0bf05a660e75c24ab3f5a28eeba45476f31
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-10-31 19:51:49 -0300

    feat: RTK config
    Adding Redux Toolkit installation and configuration, and also leaving a counter slice example and the API query structure

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,7 @@
 require('express-async-errors');
 const express = require('express');
 const errorMiddleware = require('./middlewares/error.middleware');
+const cors = require('cors');
 const loginRouter = require('./routers/login.routes');
 const userRouter = require('./routers/user.routes');
 const productRouter = require('./routers/product.routes');
@@ -11,6 +12,8 @@ const app = express();
 app.use(express.static('public'));
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/login', loginRouter);
 
