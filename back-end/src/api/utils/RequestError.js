22be8f60e54735e2f430586e8bf52e90ae259556
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-10-30 22:29:49 -0300

    feat(RequestError): created new static methods

--- back-end/src/api/utils/RequestError.js
+++ back-end/src/api/utils/RequestError.js
@@ -6,20 +6,28 @@ class RequestError extends Error {
     this.statusCode = statusCode;
   }
 
-  static invalidEmail() {
-    return new RequestError('Invalid email.', StatusCodes.BAD_REQUEST);
-  }
+  // static invalidEmail() {
+  //   return new RequestError('Invalid email.', StatusCodes.BAD_REQUEST);
+  // }
 
-  static invalidPassword() {
-    return new RequestError('Invalid password.', StatusCodes.BAD_REQUEST);
-  }
+  // static invalidPassword() {
+  //   return new RequestError('Invalid password.', StatusCodes.BAD_REQUEST);
+  // }
 
-  static userAlreadyRegistered() {
-    return new RequestError('User already registered', StatusCodes.BAD_REQUEST);
+  static emailAlreadyRegistered() {
+    return new RequestError('Email already registered.', StatusCodes.CONFLICT);
   }
 
   static userNotFound() {
-    return new RequestError('User not found', StatusCodes.NOT_FOUND);
+    return new RequestError('User not found.', StatusCodes.NOT_FOUND);
+  }
+  
+  static productNotFound() {
+    return new RequestError('Product not found.', StatusCodes.NOT_FOUND);
+  }
+
+  static invalidToken() {
+    return new RequestError('Invalid token.', StatusCodes.UNAUTHORIZED);
   }
 }
 
