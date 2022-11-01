2b74fcb48427b6fbb6285ad1ce2219b61ee10cd8
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-11-01 13:02:08 -0300

    fix: changed error middlware import path

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,6 +1,6 @@
 require('express-async-errors');
 const express = require('express');
-const errorMiddleware = require('./middlewares/error.middleware');
+const errorMiddleware = require('./middleware/error.middleware');
 const loginRouter = require('./routers/login.routes');
 const userRouter = require('./routers/user.routes');
 const productRouter = require('./routers/product.routes');
