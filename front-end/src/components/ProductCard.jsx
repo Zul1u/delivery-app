85b80353681d5258e75dbac435ff98fcae7fc024
samuel5g <samuelmonteiro21@gmail.com>
2022-11-01 16:02:03 -0300

    feat: Adds Product card and Order card unfinished

--- front-end/src/components/ProductCard.jsx
+++ front-end/src/components/ProductCard.jsx
@@ -1,1 +1,52 @@
-// Produtos presentes apenas na página de produtos
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ProductCard({ id, name, price, urlImage }) {
+  return (
+    <div>
+      <span data-test-id={ `customer_products__element-card-price-${id}` }>
+        R$
+        {price}
+      </span>
+
+      <img src={ urlImage } alt="" />
+
+      <span data-test-id={ `customer_products__element-card-title-${id}` }>
+        { name }
+      </span>
+
+      <div>
+        <input
+          type="button"
+          name="decreaseQty"
+          value="-"
+        />
+
+        <input type="text" name="qty" id="qty" value="0" />
+
+        <input
+          type="button"
+          name="increaseQty"
+          value="+"
+        />
+      </div>
+
+    </div>
+  );
+}
+
+ProductCard.propTypes = {
+  id: PropTypes.number,
+  name: PropTypes.string,
+  price: PropTypes.string,
+  urlImage: PropTypes.string,
+};
+
+ProductCard.defaultProps = {
+  id: PropTypes.number,
+  name: PropTypes.string,
+  price: PropTypes.string,
+  urlImage: PropTypes.string,
+};
+
+export default ProductCard;
