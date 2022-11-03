5f0ba969dc23f3bca736b36aeeb1ef9a74eb716c
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-11-03 16:09:14 -0300

    feat: query field test
    they'll be deleted on the next commit and shall not be merged, only for record purposes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+import FieldTest from './components/QueryFieldTest';
 import LoginPage from './pages/Login';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <Navigate to="/login" /> } />
+      {/* <Route path="/" element={ <Navigate to="/login" /> } /> */}
+      <Route path="/" element={ <FieldTest/> } />
       <Route path="/login" element={ <LoginPage /> } />
     </Routes>
   );
