9f7fa64ce7ecc46a792cbf7c826d7bf46588ab7e
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-11-03 16:10:02 -0300

    update: removing query field test

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
-import FieldTest from './components/QueryFieldTest';
 import LoginPage from './pages/Login';
 
 function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={ <Navigate to="/login" /> } /> */}
-      <Route path="/" element={ <FieldTest/> } />
+      <Route path="/" element={ <Navigate to="/login" /> } />
       <Route path="/login" element={ <LoginPage /> } />
     </Routes>
   );
