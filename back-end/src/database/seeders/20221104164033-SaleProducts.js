360007c5d23d0d8ef418ef3d0c7b85ba7e864195
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-11-05 00:03:28 -0300

    feat: created seeder for sale_products table

---
+++ back-end/src/database/seeders/20221104164033-SaleProducts.js
@@ -0,0 +1,32 @@
+module.exports = {
+  up: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkInsert('sale_products',
+      [{
+        sale_id: 1,
+        product_id: 2,
+        quantity: 3,
+      },{
+        sale_id: 1,
+        product_id: 6,
+        quantity: 1,
+      },{
+        sale_id: 2,
+        product_id: 5,
+        quantity: 2,
+      },{
+        sale_id: 2,
+        product_id: 9,
+        quantity: 1,
+      },{
+        sale_id: 2,
+        product_id: 7,
+        quantity: 9,
+      },
+      
+      ], { timestamps: false });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkDelete('sale_products', null, {});
+  },
+};
