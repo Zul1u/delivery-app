235e447a73f50b10a8697b859bb61a55ceb2684c
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-11-07 14:40:50 -0300

    lint: solving issues

--- front-end/src/components/List.jsx
+++ front-end/src/components/List.jsx
@@ -1,47 +1,45 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
-import DELIVERY_API from "../redux/services/api.fetch";
+import DELIVERY_API from '../redux/services/api.fetch';
 
-const List = ({ type, data, checkout}) => {
+function List({ type, data, checkout }) {
   const [deleteUser] = DELIVERY_API.deleteUser();
 
   const removeUser = ({ target: { parentNode } }) => {
     const row = parentNode;
     deleteUser(row.parentNode.id);
     row.parentNode.remove();
-  }
+  };
 
   const removeItem = ({ target: { parentNode } }) => {
     const row = parentNode;
     row.parentNode.remove();
-  }
+  };
 
-  const removeButton = (name, onClick) => {
-    return (
-      <button
-        type="button"
-        onClick={ onClick }
-      >
-        { name }
-      </button>
-    )
-  }
+  const removeButton = (name, onClick) => (
+    <button
+      type="button"
+      onClick={ onClick }
+    >
+      { name }
+    </button>
+  );
 
   const translateRole = (role) => {
     switch (role) {
-      case 'administrator':
-        return 'Admin';
+    case 'administrator':
+      return 'Admin';
 
-      case 'seller':
-        return 'P. Vendedora';
+    case 'seller':
+      return 'P. Vendedora';
 
-      case 'customer':
-        return 'Cliente';
-    
-      default:
-        break;
+    case 'customer':
+      return 'Cliente';
+
+    default:
+      break;
     }
-  }
+  };
 
   const userList = (
     <table>
@@ -57,7 +55,7 @@ const List = ({ type, data, checkout}) => {
       <tbody>
         {
           data.map((user, index) => (
-            <tr key={user.id} id={user.id} >
+            <tr key={ user.id } id={ user.id }>
               <td>{index + 1}</td>
               <td>{user.name}</td>
               <td>{user.email}</td>
@@ -72,49 +70,52 @@ const List = ({ type, data, checkout}) => {
 
   const productList = (
     <table>
-    <thead>
-      <tr>
-        <th>Item</th>
-        <th>Descrição</th>
-        <th>Quantidade</th>
-        <th>Valor Unitário </th>
-        <th>Sub-total</th>
-        {checkout ? (<th>Remover item</th>) : null}
-      </tr>
-    </thead>
-    <tbody>
-      {
-        data.map((item, index) => (
-          <tr key={item.id} id={item.id} >
-            <td>{index + 1}</td>
-            <td>{item.name}</td>
-            <td>{item.quantity}</td>
-            <td>{item.unitPrice}</td>
-            <td>{item.unitPrice * item.quantity}</td>
-            {checkout ? (<td>{removeButton('Remover', removeItem)}</td>) : null}
-          </tr>
-        ))
-      }
-    </tbody>
-  </table>
+      <thead>
+        <tr>
+          <th>Item</th>
+          <th>Descrição</th>
+          <th>Quantidade</th>
+          <th>Valor Unitário </th>
+          <th>Sub-total</th>
+          {checkout ? (<th>Remover item</th>) : null}
+        </tr>
+      </thead>
+      <tbody>
+        {
+          data.map((item, index) => (
+            <tr key={ item.id } id={ item.id }>
+              <td>{index + 1}</td>
+              <td>{item.name}</td>
+              <td>{item.quantity}</td>
+              <td>{item.unitPrice}</td>
+              <td>{item.unitPrice * item.quantity}</td>
+              {checkout ? (<td>{removeButton('Remover', removeItem)}</td>) : null}
+            </tr>
+          ))
+        }
+      </tbody>
+    </table>
   );
 
   switch (type) {
-    case 'user':
-      return userList;
+  case 'user':
+    return userList;
 
-    case 'product':
-      return productList;
-  
-    default:
-      null
-      break;
+  case 'product':
+    return productList;
+
+  default:
+    return <div />;
   }
-};
+}
 
 List.propTypes = {
   type: PropTypes.string.isRequired,
-  data: PropTypes.arrayOf(PropTypes.any).isRequired,
+  data: PropTypes.arrayOf(
+    PropTypes.objectOf(
+      PropTypes.string.isRequired || PropTypes.number.isRequired,
+    ).isRequired,
+  ).isRequired,
   checkout: PropTypes.bool.isRequired,
 };
 
