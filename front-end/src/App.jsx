f949f0214b3ce3b02ad52635f63c7c2758d836b0
Caminha <gabrielcaminha255@gmail.com>
2022-11-07 19:20:29 -0300

    feat: added Checkout page
    Co-authored-by:  Caio de Sousa <sshcaio@users.noreply.github.com>

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/Login';
-import Products from './pages/Products';
 import RegisterPage from './pages/Register';
+import Products from './pages/Products';
+import CheckoutPage from './pages/Checkout';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path="/login" element={ <LoginPage /> } />
       <Route path="/register" element={ <RegisterPage /> } />
       <Route path="customer/products" element={ <Products /> } />
+      <Route path="customer/checkout" element={ <CheckoutPage /> } />
     </Routes>
   );
 }
--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -1,1 +1,34 @@
-// Presente somente para o cliente
+import React, { useState, useEffect } from 'react';
+import Header from '../components/Header';
+import FinishOrderForm from '../components/FinishOrderForm';
+import List from '../components/List';
+import StorageManager from '../utils/StorageManager';
+
+export default function CheckoutPage() {
+  const [productsObject, setProductsObject] = useState({});
+  const [userId, setUserId] = useState(0);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const { id } = StorageManager.loadUser();
+    setUserId(id);
+    const loadedProducts = StorageManager.loadCart();
+    setProducts(loadedProducts);
+  }, []);
+
+  useEffect(() => {
+    const newIdObject = {};
+    products.forEach((prod) => {
+      newIdObject[prod.id] = prod.id;
+    });
+    setProductsObject({ ...newIdObject });
+  }, [products]);
+
+  return (
+    <div>
+      <Header />
+      <List type="product" data={ products } checkout />
+      <FinishOrderForm userId={ userId } products={ productsObject } />
+    </div>
+  );
+}
