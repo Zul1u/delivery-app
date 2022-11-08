b08a12ceafb46b63e1c238c29ee9e03e51ec36db
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-11-07 22:35:18 -0300

    feat: created OrderDetails page

--- front-end/src/pages/OrderDetails.jsx
+++ front-end/src/pages/OrderDetails.jsx
@@ -1,3 +1,51 @@
-// Detalhes da venda, quase idêntica em
-// Order Details do vendedor
-// Order Details do cliente
+import { useParams } from 'react-router-dom';
+import Header from '../components/Header';
+import List from '../components/List';
+import OrderDetailsHeader from '../components/OrderDetailsHeader';
+import formatDate from '../helpers/formatDate';
+import DELIVERY_API from '../redux/services/api.fetch';
+import StorageManager from '../utils/StorageManager';
+
+function OrderDetails() {
+  const { orderId } = useParams();
+  const { data: order, isLoading: saleLoading } = DELIVERY_API.getSaleById(orderId);
+  const { data: statuses, isLoading: statsLoading } = DELIVERY_API.getSaleStatuses();
+  const [updateSale] = DELIVERY_API.updateSale();
+  const { role } = StorageManager.loadUser();
+
+  // func(1, 'Entregue');
+
+  return (
+    <>
+      <button
+        type="button"
+        onClick={ () => func({ id: 1, status: 'Entregue' }) }
+      >
+        HA
+      </button>
+      <Header />
+      {(saleLoading || statsLoading) ? (
+        <p>Loading</p>
+      ) : (
+        <>
+          <OrderDetailsHeader
+            id={ orderId }
+            saleStatus={ order?.status || '' }
+            statuses={ statuses }
+            date={ order ? formatDate(order.saleDate) : '' }
+            sellerName={ role === 'customer' ? order.seller.name : '' }
+            updateSale={ updateSale }
+          />
+          <List
+            type="product"
+            data={ order.products }
+            checkout={ false }
+          />
+        </>
+      )}
+
+    </>
+  );
+}
+
+export default OrderDetails;
