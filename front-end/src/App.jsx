51a2717272fdd6f3ec81b456146559d0ba9b4e68
DanielErhardt <daniel.erhcardoso@gmail.com>
2022-11-07 21:17:08 -0300

    feat: added routes for order details page

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/Login';
+import OrderDetails from './pages/OrderDetails';
 import Products from './pages/Products';
 import RegisterPage from './pages/Register';
 
@@ -11,7 +12,9 @@ function App() {
       <Route path="/" element={ <Navigate to="/login" /> } />
       <Route path="/login" element={ <LoginPage /> } />
       <Route path="/register" element={ <RegisterPage /> } />
-      <Route path="customer/products" element={ <Products /> } />
+      <Route path="/customer/products" element={ <Products /> } />
+      <Route path="/seller/orders/:orderId" element={ <OrderDetails /> } />
+      <Route path="/customer/orders/:orderId" element={ <OrderDetails /> } />
     </Routes>
   );
 }
