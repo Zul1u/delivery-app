b5eeafa01ea39f3a1e8c2d849a90320de5cd37f3
Arthur <martinsarthur07@gmail.com>
2023-02-07 20:06:21 -0300

    style: oderDetails screen

--- front-end/src/components/OrderDetailsLabel.jsx
+++ front-end/src/components/OrderDetailsLabel.jsx
@@ -6,69 +6,73 @@ function OrderDetailsLabel(
 ) {
   const [status, setStatus] = useState(saleStatus);
   return (
-    <div>
+    <div className="order-info-container">
       {/* Número do pedido */}
-      <h3 data-testid={ `${testPrefix}element-order-details-label-order-id` }>
-        {`PEDIDO${id}`}
-      </h3>
+      <div className="order-info order-info-header">
+        <h3 data-testid={ `${testPrefix}element-order-details-label-order-id` }>
+          {`PEDIDO${id}`}
+        </h3>
 
-      {/* Nome do vendedor */}
-      { sellerName && (
-        <p data-testid={ `${testPrefix}element-order-details-label-seller-name` }>
-          {`P. Vend: ${sellerName}`}
+        {/* Nome do vendedor */}
+        {sellerName && (
+          <p data-testid={ `${testPrefix}element-order-details-label-seller-name` }>
+            {`P. Vend: ${sellerName}`}
+          </p>
+        )}
+      </div>
+      <div className="order-info order-info-body">
+        {/* Data do pedido */}
+        <p data-testid={ `${testPrefix}element-order-details-label-order-date` }>
+          {date}
         </p>
-      )}
 
-      {/* Data do pedido */}
-      <p data-testid={ `${testPrefix}element-order-details-label-order-date` }>
-        {date}
-      </p>
-
-      {/* Status atual do pedido */}
-      <p data-testid={ `${testPrefix}element-order-details-label-delivery-status` }>
-        {status}
-      </p>
-
-      {/* Renderização condicional dos botões que alteram o estado do pedido */}
-      { sellerName ? (
-        <button
-          data-testid={ `${testPrefix}button-delivery-check` }
-          type="button"
-          onClick={ () => {
-            updateSale({ id, status: statuses.delivered });
-            setStatus(statuses.delivered);
-          } }
-          disabled={ status !== statuses.inTransit }
-        >
-          MARCAR COMO ENTREGUE
-        </button>
-      ) : (
-        <>
+        {/* Status atual do pedido */}
+        <p data-testid={ `${testPrefix}element-order-details-label-delivery-status` }>
+          {status}
+        </p>
+      </div>
+      <div className="order-info-btn">
+        {/* Renderização condicional dos botões que alteram o estado do pedido */}
+        {sellerName ? (
           <button
-            data-testid={ `${testPrefix}button-preparing-check` }
+            data-testid={ `${testPrefix}button-delivery-check` }
             type="button"
-            disabled={ status !== statuses.pending }
             onClick={ () => {
-              updateSale({ id, status: statuses.preparing });
-              setStatus(statuses.preparing);
+              updateSale({ id, status: statuses.delivered });
+              setStatus(statuses.delivered);
             } }
+            disabled={ status !== statuses.inTransit }
           >
-            PREPARAR PEDIDO
+            MARCAR COMO ENTREGUE
           </button>
+        ) : (
+          <>
+            <button
+              data-testid={ `${testPrefix}button-preparing-check` }
+              type="button"
+              disabled={ status !== statuses.pending }
+              onClick={ () => {
+                updateSale({ id, status: statuses.preparing });
+                setStatus(statuses.preparing);
+              } }
+            >
+              PREPARAR PEDIDO
+            </button>
 
-          <button
-            data-testid={ `${testPrefix}button-dispatch-check` }
-            type="button"
-            disabled={ status !== statuses.preparing }
-            onClick={ () => {
-              updateSale({ id, status: statuses.inTransit });
-              setStatus(statuses.inTransit);
-            } }
-          >
-            SAIU PARA ENTREGA
-          </button>
-        </>
-      )}
+            <button
+              data-testid={ `${testPrefix}button-dispatch-check` }
+              type="button"
+              disabled={ status !== statuses.preparing }
+              onClick={ () => {
+                updateSale({ id, status: statuses.inTransit });
+                setStatus(statuses.inTransit);
+              } }
+            >
+              SAIU PARA ENTREGA
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
